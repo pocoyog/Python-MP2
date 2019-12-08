01db8b64a2bb02dfd4e670daf3f85ac432828b97
pocoyog <57284316+pocoyog@users.noreply.github.com>
2019-12-08 16:26:30 +0800

    Add files via upload

---
+++ MP2.py
@@ -0,0 +1,35 @@
+import math as mt
+import numpy as np
+
+print('         Machine Problem 2 (Python):          ')
+print('Input the corresponding needed values in terms of Integers')
+print('')
+
+x1 = float(input('Value of x1: '))
+x2 = float(input('Value of x2: '))
+x3 = float(input('Value of x3: '))
+y1 = float(input('Value of y1: '))
+y2 = float(input('Value of y2: '))
+y3 = float(input('Value of y3: '))
+
+x1a = x1**2 + y1**2
+x2a = x2**2 + y2**2
+x3a = x3**2 + y3**2
+    
+C = np.array([(x1, y1, 1), (x2, y2, 1), (x3, y3, 1)])
+D = -np.array([(x1a, y1, 1), (x2a, y2, 1), (x3a, y3, 1)])
+E = np.array([(x1a, x1, 1), (x2a, x2, 1), (x3a, x3, 1)])
+F = -np.array([(x1a, x1, y1), (x2a, x2, y2), (x3a, x3, y3)])
+    
+h = -((round(np.linalg.det(D))) / (2*round(np.linalg.det(C))))
+k = -((round(np.linalg.det(E))) / (2*round(np.linalg.det(C))))
+    
+D_val = (round(np.linalg.det(D)))/ (round(np.linalg.det(C)))
+E_val = (round(np.linalg.det(E))) / (round(np.linalg.det(C)))
+F_val = (round(np.linalg.det(F))) / (round(np.linalg.det(C)))
+    
+r = round(mt.sqrt((h - x1)**2 + (k - y1)**2))
+    
+print('Center: (',h,',',k,')')
+print('Radius: r = ',r,' units')
+print('Vector: [',D_val,',',E_val,',',F_val,']')
